2d97f6160bc863cb2ae78dd708f9d1f8251951b9
Jack <46580372+jedwards2@users.noreply.github.com>
2022-10-20 16:50:19 -0400

    class based

---
+++ tictactoe/Board.cpp
@@ -0,0 +1,91 @@
+#include "Board.hpp"
+
+Board::Board(){
+  //set all squares to _ state
+  for (int i{0}; i<m_board.size(); i++){
+    for (int q{0}; q<m_board.size(); q++){
+      m_board[i][q] = '_';
+    }
+  }
+}
+
+void Board::printBoard(){
+  for (int i{0}; i<m_board.size(); i++){
+    for (int q{0}; q<m_board.size(); q++){
+      std::cout << m_board[i][q] << " ";
+    }
+    std::cout << "\n";
+  }
+}
+
+void Board::setBoard(std::array<int, 2>& guessArray, int currentPlayer){
+  if (currentPlayer){
+    m_board[guessArray[0]][guessArray[1]] = 'O';
+  } else {
+    m_board[guessArray[0]][guessArray[1]] = 'X';
+  }
+}
+
+
+bool Board::checkRow(const std::array<char, BOARD_SIZE>& row){
+  const char check{row[0]};
+  if (check == '_'){
+    return false;
+  }
+  for (int i{0}; i<row.size(); i++){
+    if (row[i] != check){
+      return false;
+    }
+  }
+  return true;
+}
+
+bool Board::checkColumn(int colNum){
+  const char check{m_board[colNum][0]};
+  if (check == '_'){
+    return false;
+  }
+  for (int i{0}; i<m_board.size(); i++){
+    if (m_board[i][colNum] != check){
+      return false;
+    }
+  }
+  return true;
+}
+
+bool Board::checkLeftDiagonal(){
+  const char check{m_board[0][0]};
+  if (check == '_'){
+    return false;
+  }
+  for (int i{0}; i<m_board.size(); i++){
+    if (m_board[i][i] != check){
+      return false;
+    }
+  }
+  return true;
+}
+
+bool Board::checkRightDiagonal(){
+  const char check { m_board[m_board.size() - 1][0] };
+  if (check == '_'){
+    return false;
+  }
+  for (int i{0}; i<m_board.size(); i++){
+    if (m_board[(m_board.size() - 1) - i][i] != check){
+      return false;
+    }
+  }
+  return true;
+}
+
+void Board::checkIfWon(){
+  for (int i{0}; i<BOARD_SIZE; i++){
+    if (checkRow(m_board[i]) || checkColumn(i) || checkLeftDiagonal() || checkRightDiagonal()){
+      gameWon = true;
+      return;
+    }
+  }
+  gameWon = false;
+}
+
--- tictactoe/main.cpp
+++ tictactoe/main.cpp
@@ -1,43 +1,26 @@
-#include <array>
 #include <vector>
-#include <iostream>
 #include <iterator>
-
-//set compile-time const for board size, does not work due to check conditions
-constexpr int BOARD_SIZE {2};
+#include "Board.hpp"
 
 //forward declarations ------------------------------------------------
-void printBoard(const std::array<std::array<char, BOARD_SIZE>, BOARD_SIZE>& board);
-void setBoard(std::array<std::array<char, BOARD_SIZE>, BOARD_SIZE>& board, std::array<int, 2>& guessArray, int currentPlayer);
 std::array<int, 2> getGuessArray(std::vector<std::array<int, 2>>& prevGuesses);
 int getIndividualGuess();
 int switchPlayer(int currentPlayer);
-bool checkIfWon(const std::array<std::array<char, BOARD_SIZE>, BOARD_SIZE>& board, bool gameWon);
-bool checkColumn(const std::array<std::array<char, BOARD_SIZE>, BOARD_SIZE>& board, int colNum);
-bool checkRow(const std::array<char, BOARD_SIZE>& row);
-bool checkRightDiagonal(const std::array<std::array<char, BOARD_SIZE>, BOARD_SIZE>& board);
-bool checkLeftDiagonal(const std::array<std::array<char, BOARD_SIZE>, BOARD_SIZE>& board);
 bool checkIfGuessIsValid(int guess1, int guess2, std::vector<std::array<int, 2>>& prevGuesses);
 
 int main(int argc, const char * argv[]) {
   //initial multidimensional board array
-  std::array<std::array<char, BOARD_SIZE>, BOARD_SIZE> board {};
-  
+
   //array of 2 ints for previous guesses
   std::vector<std::array<int, 2>> prevGuesses;
   
-  bool gameWon {false};
+  Board gameBoard {};
   int currentPlayer{0};
+
   
-  //set all squares to _ state
-  for (int i{0}; i<board.size(); i++){
-    for (int q{0}; q<board.size(); q++){
-      board[i][q] = '_';
-    }
-  }
-  
-  while (!gameWon){
-    printBoard(board);
+  while (!gameBoard.isGameWon()){
+    gameBoard.printBoard();
+
     std::cout << "Current State of the Board" << "\n";
     
     //get array of 2 integers from player
@@ -45,10 +28,10 @@ int main(int argc, const char * argv[]) {
     prevGuesses.push_back(guess);
     
     //update the board with currentPlayer's mark
-    setBoard(board, guess, currentPlayer);
+    gameBoard.setBoard(guess, currentPlayer);
     
     //update current state of game
-    gameWon = checkIfWon(board, gameWon);
+    gameBoard.checkIfWon();
     
     //update the current Player
     currentPlayer = switchPlayer(currentPlayer);
@@ -92,84 +75,8 @@ std::array<int, 2> getGuessArray(std::vector<std::array<int, 2>>& prevGuesses){
   return std::array<int, 2>{ guess1, guess2};
 }
 
-void printBoard(const std::array<std::array<char, BOARD_SIZE>, BOARD_SIZE>& board){
-  for (int i{0}; i<board.size(); i++){
-    for (int q{0}; q<board.size(); q++){
-      std::cout << board[i][q] << " ";
-    }
-    std::cout << "\n";
-  }
-}
-
-void setBoard(std::array<std::array<char, BOARD_SIZE>, BOARD_SIZE>& board, std::array<int, 2>& guessArray, int currentPlayer){
-  if (currentPlayer){
-    board[guessArray[0]][guessArray[1]] = 'O';
-  } else {
-    board[guessArray[0]][guessArray[1]] = 'X';
-  }
-}
 
 int switchPlayer(int currentPlayer){
   return currentPlayer ? 0 : 1;
 }
 
-bool checkIfWon(const std::array<std::array<char, BOARD_SIZE>, BOARD_SIZE>& board, bool gameWon){
-  for (int i{0}; i<BOARD_SIZE; i++){
-    if (checkRow(board[i]) || checkColumn(board, i) || checkLeftDiagonal(board) || checkRightDiagonal(board)){
-      return true;
-    }
-  }
-  return false;
-}
-
-bool checkRow(const std::array<char, BOARD_SIZE>& row){
-  const char check{row[0]};
-  if (check == '_'){
-    return false;
-  }
-  for (int i{0}; i<row.size(); i++){
-    if (row[i] != check){
-      return false;
-    }
-  }
-  return true;
-}
-
-bool checkColumn(const std::array<std::array<char, BOARD_SIZE>, BOARD_SIZE>& board, int colNum){
-  const char check{board[colNum][0]};
-  if (check == '_'){
-    return false;
-  }
-  for (int i{0}; i<board.size(); i++){
-    if (board[i][colNum] != check){
-      return false;
-    }
-  }
-  return true;
-}
-
-bool checkLeftDiagonal(const std::array<std::array<char, BOARD_SIZE>, BOARD_SIZE>& board){
-  const char check{board[0][0]};
-  if (check == '_'){
-    return false;
-  }
-  for (int i{0}; i<board.size(); i++){
-    if (board[i][i] != check){
-      return false;
-    }
-  }
-  return true;
-}
-
-bool checkRightDiagonal(const std::array<std::array<char, BOARD_SIZE>, BOARD_SIZE>& board){
-  const char check { board[board.size() - 1][0] };
-  if (check == '_'){
-    return false;
-  }
-  for (int i{0}; i<board.size(); i++){
-    if (board[(board.size() - 1) - i][i] != check){
-      return false;
-    }
-  }
-  return true;
-}
